fe79bf461bc1eb8eb6af12bb07653e505a57b8bd
Alexander <orientstar96@gmail.com>
2022-09-11 21:55:53 +0500

    7.4 Подгонка под pp страницы каталога

--- source/js/app.js
+++ source/js/app.js
@@ -1,6 +1,6 @@
 const toggleMenuButton = document.querySelector(".main-navigation__toggle");
 const menu = document.querySelector(".site-navigation");
-const nav = document.querySelector(".main-navigation"); // сама навигация
+const nav = document.querySelector(".main-navigation");
 
 nav.classList.remove("main-navigation--nojs");
 
